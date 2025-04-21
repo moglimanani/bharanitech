7d0e6e7356fa3baffb3d34e42a07dba17fe5303e
Manimaran <moglimani@gmail.com>
2025-04-21 15:47:38 +0530

    style changes

--- baranitech/src/theme.ts
+++ baranitech/src/theme.ts
@@ -3,6 +3,9 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
+    background:{
+        default: '#bce0ed'
+    },
     primary: {
       main: '#6a1b9a', // Purple theme
     },
