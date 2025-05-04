5a1799f0b38e443bba2a881eed5bbaa6f0078fa5
Manimaran Uthirapathy <moglimani@gmail.com>
2025-05-04 22:51:29 +0530

    home page

--- baranitech/src/pages/styles.tsx
+++ baranitech/src/pages/styles.tsx
@@ -1,7 +1,6 @@
-import { Box, styled, Typography } from "@mui/material";
+import { Box, List, ListItemText, styled, Typography } from "@mui/material";
 
 export const BodyPara1Styled = styled(Typography)(({theme})=>({
-  backgroundColor: theme.palette.appBarColour.main,
   padding: "10px 10px",
   textAlign: 'left',
   alignItems: 'flex-start',
@@ -11,4 +10,14 @@ export const BodyPara1Styled = styled(Typography)(({theme})=>({
 
 export const PageWrapperStyled = styled(Box)(({theme})=>({
   padding: '16px 16px 16px 20px'
+}))
+
+export const ListItemTextStyled = styled(ListItemText)(({theme})=>({
+  textAlign: 'left'
+}))
+
+export const ListStyled = styled(List)(({theme})=>({
+  textAlign: 'left',
+  paddingLeft: '20px',
+  color: theme.palette.common.white
 }))
