0a745804007208a9c3a645ff425c559ad8713aac
sethu666 <vinosethu@gmail.com>
2025-05-11 13:37:43 +0530

    about page  update

--- baranitech/src/pages/AboutUsPage.tsx
+++ baranitech/src/pages/AboutUsPage.tsx
@@ -1,6 +1,26 @@
+import { AboutUsParaStyled, AboutUsStyled, AboutUsTitleStyled } from "./styles";
 
 export default function AboutUsPage() {
   return (
-    <div>AboutUsPage</div>
+    <AboutUsStyled>
+      <AboutUsTitleStyled>
+         AboutUsPage
+      </AboutUsTitleStyled>
+      <AboutUsParaStyled>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </AboutUsParaStyled>
+      <AboutUsParaStyled>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </AboutUsParaStyled>
+      <AboutUsParaStyled>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </AboutUsParaStyled>
+      <AboutUsParaStyled>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </AboutUsParaStyled>
+      <AboutUsParaStyled>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+      </AboutUsParaStyled>
+    </AboutUsStyled>
   )
 }
--- baranitech/src/pages/styles.tsx
+++ baranitech/src/pages/styles.tsx
@@ -50,3 +50,38 @@ export const TwoColumnStyled = styled(Grid)(({theme})=>({
     padding: '20px',
  }
 }))
+
+// AboutUs
+
+export const AboutUsStyled = styled('div')(({theme})=>({
+  margin: '30px',
+  padding: "0px 20px 0px",
+  background: '#ffffff',
+  borderRadius: '20px',
+  textAlign: 'left',
+  [theme.breakpoints.down('sm')]: {
+    padding: '20px',
+ }
+}))
+
+export const AboutUsTitleStyled = styled('h2')(({theme})=>({
+ // background: 'yellow',
+  padding: '20px 0px 0px',
+  color: '#127B93',
+  fontWeight: 'bold',
+  fontSize: '1.3em',
+  [theme.breakpoints.down('sm')]: {
+    padding: '20px',
+ }
+}))
+
+export const AboutUsParaStyled = styled('p')(({theme})=>({
+  // background: 'yellow',
+   margin: '0',
+   padding: '0 0 20px',
+   color: '#4d4d4d',
+   fontSize: '0.88em',
+   [theme.breakpoints.down('sm')]: {
+     padding: '20px',
+  }
+ }))
