95a76e07caa5d8830a424a8ad94254cd47de688f
sethu666 <vinosethu@gmail.com>
2025-05-12 17:02:02 +0530

    about us page

--- baranitech/src/components/ThreeColumnWhitePage/styles.tsx
+++ baranitech/src/components/ThreeColumnWhitePage/styles.tsx
@@ -1,4 +1,4 @@
-import {  Box, Grid, styled } from "@mui/material";
+import {  Box, Grid, styled, } from "@mui/material";
 
 export const BoxWhiteStyled = styled(Box)(({theme})=>({
   background: '#fff',
