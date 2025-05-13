664a57826ec87ca33d8fbcf44539b31b4e001ee6
sethu666 <vinosethu@gmail.com>
2025-05-13 07:35:08 +0530

    login  page

---
+++ baranitech/src/pages/LoginPage/styles.tsx
@@ -0,0 +1,49 @@
+
+import {  Grid, styled, Avatar, Button} from "@mui/material";
+
+export const LoginWrapperStyled = styled(Grid)(({theme})=>({
+   margin: '30px',
+   padding: '15px 20px',
+   backgroundColor: '#ffffff',
+   borderRadius: '20px',
+   [theme.breakpoints.down('sm')]: {
+    margin: '20px',
+    padding: '0',
+ },
+ [theme.breakpoints.down('sm')]: {
+ '& > div > div': {
+    marginTop: '0px',
+    padding: '0px'
+ }
+}
+  
+ }))
+
+ export const LoginWrapperImageStyled = styled(Grid)(({theme})=>({
+  // background: 'yellow',
+  margin: '30px',
+  padding: '15px 20px',
+  textAlign: 'right',
+  'img': {
+    width: '100%',
+  },
+  [theme.breakpoints.down('sm')]: {
+    margin: '0',
+    padding: '0',
+    display: 'none',
+ }
+ 
+}))
+
+export const LoginWrapperIconStyled = styled(Avatar)(({theme})=>({
+  color: '#4d4d4d',
+  background: '#fff !important' ,
+}))
+
+export const LoginWrapperButtonStyled = styled(Button)(({theme})=>({
+  background: '#127B93' ,
+  borderRadius: '20px'
+
+}))
+
+ 
