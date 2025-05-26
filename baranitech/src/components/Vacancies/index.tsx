63bd9a0b27bedbf5730518a74f6b26c2e9e44171
Manimaran Uthirapathy <moglimani@gmail.com>
2025-05-26 22:34:56 +0530

    vacancy integration

--- baranitech/src/components/Vacancies/index.tsx
+++ baranitech/src/components/Vacancies/index.tsx
@@ -1,114 +1,48 @@
 import { Grid } from "@mui/material";
 import WorkIcon from '@mui/icons-material/Work';
-import { BadgeStyled, GridColorStyled, GridParaStyled, IconBadgeStyled, IconStyled, Item, ItemRight, JobsStyled, VacanciesStyled } from "./styles";
+import { GridColorStyled, IconBadgeStyled, ItemRight, JobsStyled, VacanciesStyled } from "./styles";
+import { useJobCategories } from "../../contexts/jobCategoryContext";
+import { useEffect, useState } from "react";
+import { useAllJobs } from "../../contexts/allJobsContext";
+import { JobsType } from "../../types/jobs";
 
 function Vacancies() {
 
-    return ( 
-      <Grid container justifyContent='center'>
-         <ItemRight>
-             <VacanciesStyled variant="h5">Vacancies:  </VacanciesStyled>
-         </ItemRight>
-        <GridColorStyled spacing={2}>
-               <Grid>
-                <JobsStyled align='left'>Site Engineers  </JobsStyled>
-               </Grid>
-               <Grid>
-                   <IconBadgeStyled badgeContent={14} color="primary"  >
-                      <WorkIcon color="action"/>
-                    </IconBadgeStyled>
-               </Grid>
-         </GridColorStyled>
-         <GridColorStyled spacing={2}>
-               <Grid>
-                <JobsStyled align='left'>Technicians  </JobsStyled>
-               </Grid>
-               <Grid>
-                   <IconBadgeStyled badgeContent={10} color="primary"  >
-                      <WorkIcon color="action"/>
-                    </IconBadgeStyled>
-               </Grid>
-         </GridColorStyled>
-         <GridColorStyled spacing={2}>
-               <Grid>
-                <JobsStyled align='left'>QA  </JobsStyled>
-               </Grid>
-               <Grid>
-                   <IconBadgeStyled badgeContent={30} color="primary"  >
-                      <WorkIcon color="action"/>
-                    </IconBadgeStyled>
-               </Grid>
-         </GridColorStyled>
-         
-         
-      </Grid>
-      //   <Grid container spacing={2}>
-      //   <Grid size={{ xs: 12, sm: 4 }}>
-      //     <ItemRight>
-      //       <VacanciesStyled variant="h5">Vacancies:  </VacanciesStyled>
-      //     </ItemRight>
-      //   </Grid>
-      //   <Grid>
-      //     <Grid container>
-      //            <Grid>
-      //               <JobsStyled align='left'>Site Engineers  </JobsStyled>
-      //             </Grid>
-      //             <Grid>
-      //               <IconBadgeStyled badgeContent={14} color="primary"  >
-      //                 <WorkIcon color="action"/>
-      //               </IconBadgeStyled>
-      //             </Grid>
-      //     </Grid>
-          
-      //   </Grid>
-      //   {/* <GridParaStyled>
-      //     <Grid container spacing={2}>
-      //       <Grid size={{ xs: 4, sm: 12 }}>
-      //         <Item>
-      //           <Grid container>
-      //             <Grid size={10}>
-      //               <JobsStyled align='left'>Site Engineers  </JobsStyled>
-      //             </Grid>
-      //             <Grid size={2}>
-      //               <IconBadgeStyled badgeContent={14} color="primary"  >
-      //                 <WorkIcon color="action"/>
-      //               </IconBadgeStyled>
-      //             </Grid>
-      //           </Grid>
-      //         </Item>
-      //       </Grid>
-      //       <Grid size={{ xs: 4, sm: 12 }}>
-      //         <Item>
-      //           <Grid container>
-      //             <Grid size={10}>
-      //               <JobsStyled align='left'>Technicians </JobsStyled>
-      //             </Grid>
-      //             <Grid size={2}>
-      //               <IconBadgeStyled badgeContent={24} color="primary">
-      //               <WorkIcon color="action" />
-      //               </IconBadgeStyled>
-      //             </Grid>
-      //           </Grid>
-      //         </Item>
-      //       </Grid>
-      //       <Grid size={{ xs: 4, sm: 12 }}>
-      //         <Item>
-      //           <Grid container display={"flex"} justifyContent={"center"} alignItems={"center"}>
-      //             <Grid size={10}>
-      //               <JobsStyled align='left' >QA  </JobsStyled>
-      //             </Grid>
-      //             <Grid size={2}>
-      //               <IconBadgeStyled badgeContent={34} color="primary">
-      //               <WorkIcon color="action" />
-      //               </IconBadgeStyled>
-      //             </Grid>
-      //           </Grid>
-      //         </Item>
-      //       </Grid>
-      //     </Grid>
-      //   </GridParaStyled> */}
-      // </Grid>
-     );
+  const [jobs, setJobs] = useState<JobsType[]>([])
+  const { categories } = useJobCategories()
+  const { allJobs } = useAllJobs()
+
+  useEffect(() => {
+    if (Array.isArray(allJobs)) {
+      const sortedArr = [...allJobs].sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+      const unqiueBylatest: JobsType[] = Array.from(new Map(sortedArr?.map((data: JobsType) => [data.type, data]))?.values())
+      setJobs(unqiueBylatest)
+    }
+  }, [allJobs])
+
+
+  return (
+    <Grid container justifyContent='center'>
+      <ItemRight>
+        <VacanciesStyled variant="h5">Vacancies:  </VacanciesStyled>
+      </ItemRight>
+      {
+        jobs && categories?.map((item) => (
+          <GridColorStyled spacing={2} key={`jobCat${item.id}`}>
+            <Grid>
+              <JobsStyled align='left'>{item.title}</JobsStyled>
+            </Grid>
+            <Grid>
+              <IconBadgeStyled badgeContent={jobs?.find((jItem: JobsType) => jItem?.type === item.id)?.total_vacancy ?? 0} color="primary"  >
+                <WorkIcon color="action" />
+              </IconBadgeStyled>
+            </Grid>
+          </GridColorStyled>
+        ))
+      }
+
+    </Grid>
+  );
 }
 
 export default Vacancies;
---
+++ baranitech/src/contexts/allJobsContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { allJobsType, ApiResponse, JobCategoryType, JobsType } from '../types/jobs';
+import httpService from '../api/httpService';
+
+const AllJobsContext = createContext<allJobsType | undefined>(undefined);
+
+export const AllJobsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [allJobs, setAllJobs] = useState<JobsType[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchJobs = async () => {
+    try {
+      setLoading(true);
+      const res: ApiResponse  = await httpService.get(`${import.meta.env.VITE_API_BASE_URL}jobs`);
+      if(res.status){
+        setAllJobs(res.data);
+        setError(null);
+      }
+    } catch (err) {
+      console.error(err);
+      setError('Failed to load jobs');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetchJobs();
+    return () => {
+      controller.abort(); // Cancel fetch on unmount
+    };
+  }, []);
+
+  return (
+    <AllJobsContext.Provider value={{ allJobs, loading, error, refresh: fetchJobs }}>
+      {children}
+    </AllJobsContext.Provider>
+  );
+};
+
+export const useAllJobs = (): allJobsType => {
+  const context = useContext(AllJobsContext);
+  if (!context) {
+    throw new Error('useAllJobs must be used within a AllJobsProvider');
+  }
+  return context;
+};
